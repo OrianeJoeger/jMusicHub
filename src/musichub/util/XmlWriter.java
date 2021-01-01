08dbfa960c5a7f867c5d9bdde6501956e1aef571
Oriane Joeger <oriane.joeger@gmail.com>
2021-01-01 17:04:04 +0100

    XmlWriter  change parameters to Collections

--- src/musichub/util/XmlWriter.java
+++ src/musichub/util/XmlWriter.java
@@ -1,5 +1,7 @@
 package musichub.util;
 
+import java.util.Collection;
+
 import musichub.business.Album;
 import musichub.business.Chanson;
 import musichub.business.LivreAudio;
@@ -11,20 +13,20 @@ public class XmlWriter {
         //TODO: constructeur
     }
 
-    private void saveElements(Chanson chansons, LivreAudio livresaudio) {
+    private void saveElements(Collection<Chanson> chansons, Collection<LivreAudio> livresaudios) {
         //TODO: a des chansons et livresaudio dans le fichier elements.xml
     }
 
-    private void saveAlbums(Album albums) {
+    private void saveAlbums(Collection<Album> albums) {
         //TODO: sauvegarde des albums dans le fichier albums.xml
     }
 
-    private void savePlaylists(Playlist playlists) {
+    private void savePlaylists(Collection<Playlist> playlists) {
         //TODO: sauvegarde des playlists dans le fichier playlists.xml
     }
 
-    public void save (Chanson chanson, LivreAudio livresaudio, Album albums, Playlist playlists) {
-        this.saveElements(chanson, livresaudio);
+    public void save (Collection<Chanson> chansons, Collection<LivreAudio> livresaudios, Collection<Album> albums, Collection<Playlist> playlists) {
+        this.saveElements(chansons, livresaudios);
         this.saveAlbums(albums);
         this.savePlaylists(playlists);
     }
